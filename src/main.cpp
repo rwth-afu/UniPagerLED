ccb17a6ff5f416e43e1399875e58578d6db30f7c
Ralf Wilke <wilke@ihf.rwth-aachen.de>
2017-03-09 14:16:28 +0100

    Frist commit of software

---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include "client_ws.hpp"
+#include <wiringPi.h>
+
+
+using namespace std;
+
+typedef SimpleWeb::SocketClient<SimpleWeb::WS> WsClient;
+
+int main() {
+    
+    //Example 4: Client communication with server
+    //Possible output:
+    //Server: Opened connection 140184920260656
+    //Client: Opened connection
+    //Client: Sending message: "Hello"
+    //Server: Message received: "Hello" from 140184920260656
+    //Server: Sending message "Hello" to 140184920260656
+    //Client: Message received: "Hello"
+    //Client: Sending close connection
+    //Server: Closed connection 140184920260656 with status code 1000
+    //Client: Closed connection with status code 1000
+    WsClient client("localhost:8080/echo");
+    client.on_message=[&client](shared_ptr<WsClient::Message> message) {
+        auto message_str=message->string();
+        
+        cout << "Client: Message received: \"" << message_str << "\"" << endl;
+        
+        cout << "Client: Sending close connection" << endl;
+        client.send_close(1000);
+    };
+    
+    client.on_open=[&client]() {
+        cout << "Client: Opened connection" << endl;
+        
+        string message="Hello";
+        cout << "Client: Sending message: \"" << message << "\"" << endl;
+
+        auto send_stream=make_shared<WsClient::SendStream>();
+        *send_stream << message;
+        client.send(send_stream);
+    };
+    
+    client.on_close=[](int status, const string& /*reason*/) {
+        cout << "Client: Closed connection with status code " << status << endl;
+    };
+    
+    //See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
+    client.on_error=[](const boost::system::error_code& ec) {
+        cout << "Client: Error: " << ec << ", error message: " << ec.message() << endl;
+    };
+    
+    client.start();
+    
+    server_thread.join();
+    
+    return 0;
+}
