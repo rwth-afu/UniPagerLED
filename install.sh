9cabd9670268e24f3f2a054627029866c1e8c86c
Ralf Wilke DH3WR <dh3wr@users.noreply.github.com>
2017-08-11 19:37:22 +0200

    Deleted empty lines and moved to bash

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
-#!/bin/sh -x
-
+#!/bin/bash
 cp unipagerled.py /usr/local/bin/unipagerled.py
 cp config.py.example /etc/unipagerledconfig.py
 mkdir -p /usr/local/lib/systemd/system
 cp unipagerled.service /usr/local/lib/systemd/system/unipagerled.service
-
 systemctl daemon-reload
