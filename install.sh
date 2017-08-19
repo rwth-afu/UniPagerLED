dec9737dfcb657e8d84f734c10df05c9ace9d519
Ralf Wilke <dh3wr@darc.de>
2017-08-19 14:14:56 +0200

    Improvment of install script

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+echo "Must be run as root. Try sudo just in case!"
+
 cp unipagerled.py /usr/local/bin/unipagerled.py
 cp config.py.example /etc/unipagerledconfig.py
 mkdir -p /usr/local/lib/systemd/system
 cp unipagerled.service /usr/local/lib/systemd/system/unipagerled.service
 systemctl daemon-reload
+
+systemctl start unipagerled.service
+systemctl enable unipagerled.service
+
+echo "Install completed. Edit /etc/unipagerled.py according to your needs and run"
+echo "sudo systemctl restart unipagerled.service"
+echo "to make your changed active."
+
